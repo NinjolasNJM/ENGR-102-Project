3a6b191477574300c9e9cb35c7eb229a61cb1e88
NinjolasNJM <NinjolasNJM2003@gmail.com>
2022-11-22 17:18:51 -0600

    Test commits working in pycharm

---
+++ main.py
@@ -0,0 +1,12 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Names:        Nicholas Mirabal
+#               Eli Valentino
+#               Chris Avila
+# Section:      102-565
+# Assignment:   Project
+# Date:         6 December 2022
+
+print('something funny')
