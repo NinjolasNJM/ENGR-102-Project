7eb76462a1b437b354ccbf26174924a882e74e85
ChrisA2603 <chris.26@tamu.edu>
2022-12-04 19:01:50 -0600

    Class 'Car'

--- turtle_graphic.py
+++ turtle_graphic.py
@@ -48,35 +48,93 @@ def turn(direction):
     else:
         left(0)
 
-
-
-
-
+"""
+setup(500, 550)
+title('Kyle Field to Veterans Park')
 speed(1)
 setheading(90)
 
-forward(50)
-turn('southeast')
-forward(100)
-turn('southwest')
-forward(100)
-turn('onto')
-forward(200)
-turn('left')
-forward(100)
-turn('right')
-forward(50)
-turn('north')
-forward(300)
-turn('straight')
-forward(500)
 
-heading()
+
+turn('southeast')
+forward(20)
+turn('left')
+forward(110)
+turn('left')
+forward(30)
+turn('right')
+forward(140)
+turn('left')
+forward(20)
+turn('right')
+forward(80)
+turn('right')
+forward(40)
+
+exitonclick()
+
+"""
+
+#Make function for turning
+
+#Turtle Shape
+speed(1)
+penup()
+left(180)
+forward(200)
+left(180)
+pendown()
+forward(50)
+right(90)
+circle(50, 180)
+right(90)
+forward(100)
+right(90)
+circle(50, 180)
+
+
 
 exitonclick()
 
 
+class Car:
 
-#Make function for turning
+    def __init__(self, windowHeight=500, windowWidth=600, speed=2):
+        speed(speed)
 
-#Make function for
+    def turn(direction):
+        if direction == 'north':
+            setheading(90)  # Sets direction to north when using a objective direction
+            left(0)
+        elif direction == 'east':
+            setheading(90)
+            right(90)
+        elif direction == 'south':
+            setheading(90)
+            right(180)
+        elif direction == 'west':
+            setheading(90)
+            left(90)
+        elif direction == 'northeast':
+            setheading(90)
+            right(45)
+        elif direction == 'southeast':
+            setheading(90)
+            right(135)
+        elif direction == 'northwest':
+            setheading(90)
+            left(45)
+        elif direction == 'southwest':
+            setheading(90)
+            left(135)
+        elif direction == 'left':
+
+            left(90)
+        elif direction == 'right':
+
+            right(90)
+        elif direction == 'onto':
+
+            left(0)
+        else:
+            left(0)
