9437c21d0d34e3c6a9833162175b6221b4c25f36
NinjolasNJM <NinjolasNJM2003@gmail.com>
2022-12-06 16:52:39 -0600

    DOscstrings for main

--- main.py
+++ main.py
@@ -19,19 +19,30 @@ file1 = 'Kyle2VetPk.txt'
 file2 = 'Easterwood2Coulter.txt'
 file3 = 'Zach2StJo.txt'
 
-gui=GUI()
-file_name=gui.file
-file_name=file_name.split('\n')[0]
-s = gui.speed
+def getVars():
+    ''' @NinjolasNJM2003@tamu.edu
+
+    Creates the GUI and uses it to create the instructions
+    and speed variables.'''
+    gui=GUI()
+    file_name=gui.file
+    file_name=file_name.split('\n')[0]
+    s = gui.speed
+    return file_name, s
 
 
 def codeToRun(fileName,s):
+    '''@NinjolasNJM2003@tamu.edu
+
+    The code that runs the GUI, getting the input and
+    the turtle output. Additionally prints the steps to the console.
+    '''
     steps = Instructions(fileName)
     print(steps)
     drive = Drive(fileName, 500,600,s)
     drive.run(steps.makeInstructions())
 
-
+file_name, s = getVars()
 codeToRun(file_name,s)
 
 
